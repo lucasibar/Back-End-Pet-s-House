17d68f30a9cd88525c99c6ec8358ca9d033acf35
lisandrosz <szyzuklisandro@gmail.com>
2023-02-21 20:01:15 -0300

    coneccion funcionando

--- src/models/Pets.js
+++ src/models/Pets.js
@@ -1,55 +1,56 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-sequelize.define('Pets', {
-
-  id: {
-    type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    //allowNull: false,
-  },
-  image: {
-    type: DataTypes.STRING,
-    defaultValue: 'https://i.pinimg.com/originals/37/2c/db/372cdbb7af08ad27524ef53c53e1ba8d.jpg',
-  }, 
-  age: {
-    type: DataTypes.INTEGER 
-  },
-  description: {
-    type: DataTypes.STRING
-  },
-  size:{
-    type: DataTypes.ENUM("grande", "chico","mediano")
-  },
-  healthBook:{
-    type: DataTypes.BOOLEAN
-  }, 
-  animal:{
-    type: DataTypes.STRING
-  },
-  active:{
-    type: DataTypes.BOOLEAN,
-    defaultValue: true
-  },
-  province:{
-    type: DataTypes.STRING
-  },
-  location:{
-    type: DataTypes.STRING
-  },
-  sex:{
-    type: DataTypes.ENUM ( "masculino", "femenino")
-  }
-},
-{
-  timestamps: false,
-  createdAt: false,
-  updatedAt: false
-  });
+  sequelize.define(
+    "Pets",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        //allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        defaultValue:
+          "https://i.pinimg.com/originals/37/2c/db/372cdbb7af08ad27524ef53c53e1ba8d.jpg",
+      },
+      age: {
+        type: DataTypes.INTEGER,
+      },
+      description: {
+        type: DataTypes.STRING,
+      },
+      size: {
+        type: DataTypes.ENUM("grande", "chico", "mediano"),
+      },
+      healthBook: {
+        type: DataTypes.BOOLEAN,
+      },
+      animal: {
+        type: DataTypes.STRING,
+      },
+      active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+      province: {
+        type: DataTypes.STRING,
+      },
+      location: {
+        type: DataTypes.STRING,
+      },
+      sex: {
+        type: DataTypes.ENUM("masculino", "femenino"),
+      },
+    },
+    {
+      timestamps: false,
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
 };
-
-
