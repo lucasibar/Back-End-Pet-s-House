7bbff1bad8c03795309541dc898d5189e603c2b7
lucasibar <lucas.ibar.soy.henry@gmail.com>
2023-02-27 10:50:24 -0300

    aaa

--- src/routes/controlers/controllersFavorites/createFavorite.js
+++ src/routes/controlers/controllersFavorites/createFavorite.js
@@ -10,6 +10,7 @@ module.exports = {
         UserId:idUser,
         PetId:idPets}
     })
+    
     if(existeFavorito.length>0) return `${pet.name} ya existe en la lista de favortitos de ${user.name}`
     const newFavorite = await FavoritesPets.create()
     await pet.addUser(user.id)
