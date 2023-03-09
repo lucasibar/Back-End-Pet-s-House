18a1b0df0c69ee5b258fc9c1bf1ac9db1f869c2b
Kevin <kevinesteban2010@hotmail.com>
2023-03-08 23:41:34 -0500

    Corregido lo de busqueda

--- src/routes/controlers/controllersPets/getPetsByName.js
+++ src/routes/controlers/controllersPets/getPetsByName.js
@@ -1,11 +1,11 @@
 const { Pets } = require("../../../db");
+const { Op } = require("sequelize")
 
 module.exports = {
 getPetsByName: async function (name) {
 try{
-  const searchPetByName = await Pets.findAll({
-    where:{name}
-  });
+  console.log("name: ", name)
+  const searchPetByName = await Pets.findAll({where: {name : { [Op.iLike]: `%${name}%`}}})
   return searchPetByName
 }
 catch{throw Error (`No se encontro ninguna mascota con el nombre ${name}`)}
